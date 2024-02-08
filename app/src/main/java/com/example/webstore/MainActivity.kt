8f6fb9c17303f178c2ab8e90dd220c96bba4c024
Dmitr-Kotov <kotovdmitr79@gmail.com>
2024-02-08 22:52:58 +0300

    added a user registration screen

--- app/src/main/java/com/example/webstore/MainActivity.kt
+++ app/src/main/java/com/example/webstore/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.webstore
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val userLogin: EditText = findViewById(R.id.user_login)
+        val userEmail: EditText = findViewById(R.id.user_email)
+        val userPass: EditText = findViewById(R.id.user_pass)
+        val button: Button = findViewById(R.id.button_req)
+
+        button.setOnClickListener{
+
+        }
     }
 }
